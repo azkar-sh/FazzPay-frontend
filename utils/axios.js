2ca092f3890a4a7db0595563eb000fd90a2e461f
azkar-sh <akbar.dizir@gmail.com>
2022-10-31 10:25:20 +0700

    fix: error router

--- utils/axios.js
+++ utils/axios.js
@@ -31,10 +31,10 @@ axiosApiIntances.interceptors.response.use(
     // Any status codes that falls outside the range of 2xx cause this function to trigger
     // Do something with response error
 
-    if (error.response.status === 403) {
-      Cookies.remove("token");
-      window.location.href = "/login";
-    }
+    // if (error.response.status === 403) {
+    //   Cookies.remove("token");
+    //   window.location.href = "/login";
+    // }
     return Promise.reject(error);
   }
 );
